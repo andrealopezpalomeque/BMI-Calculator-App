8dc31b496d00c373d4dce56ede1440fdf8b61164
Andrea Lopez Palomeque <andrealopezpalomeque@gmail.com>
2023-09-14 22:42:43 -0300

    first screen values to height and weight sliders and calculate button

--- BMI Calculator App/ViewController.swift
+++ BMI Calculator App/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    
+    @IBOutlet weak var weightUISlider: UISlider!
+    @IBOutlet weak var heightUISlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func weightSlider(_ sender: UISlider) {
+        let value = Int(sender.value)
+        weightLabel.text = "\(String(value))Kg"
+    }
+    @IBAction func heightSlider(_ sender: UISlider) {
+        let value = round(sender.value * 100) / 100.0
+        heightLabel.text = "\(String(value))m"
+    }
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let weight = weightUISlider.value
+        let height = heightUISlider.value
+        
+        
+        let bmi = weight / pow(height, 2)
+        
+        print(bmi)
+    }
 }
 
